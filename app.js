d4b43652d7c2394b941ed236bfb36cced5355d60
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2026-06-16 22:13:33 +0530

    main

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const http = require("http");
 
 const socketio = require("socket.io");
 
+const server = http.createServer(app);
+
+
+const io = socketio(server);
+
 app.get('/', (req, res) => {
     res.send('Hey');
 });
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
